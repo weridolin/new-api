9302cbaa48a0e4a741717869da2db38fa70cdc27
werido <359066432@qq.com>
2026-04-18 22:02:39 +0800

    update

--- scripts/dev-entrypoint.sh
+++ scripts/dev-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "[dev] Building frontend..."
-cd /app/web
-bun install
-DISABLE_ESLINT_PLUGIN=true VITE_REACT_APP_VERSION=$(cat /app/VERSION) bun run build
-
 echo "[dev] Building backend..."
 cd /app
 VERSION=$(cat VERSION)
